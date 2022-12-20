0052d81add58b8a1b7ea64a50bb56c0e470950fd
Kevin Pham <kevinpham@Kevins-MacBook-Pro.local>
2022-12-19 21:24:58 -0600

    C key successfully makes noise. The rest only print to the console.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func cPressed(_ sender: UIButton) {
+        print("c")
+        playSound()
+    }
     
 
+    @IBAction func dPressed(_ sender: UIButton) {
+        print("d")
+    }
+    
+    
+    @IBAction func ePressed(_ sender: UIButton) {
+        print("e")
+    }
+    
+    
+    @IBAction func fPressed(_ sender: UIButton) {
+        print("f")
+    }
+    
+    
+    @IBAction func gPressed(_ sender: UIButton) {
+        print("g")
+    }
+    
+    
+    @IBAction func aPressed(_ sender: UIButton) {
+        print("a")
+    }
+    
+    
+    @IBAction func bPressed(_ sender: UIButton) {
+        print("b")
+    }
+    
+    
+    func playSound() {
+            let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+    }
+    
 }
 
