ff37f4dcf2793ded37a185246017576842fb1abd
Kevin Pham <phamthk@gmail.com>
2022-12-20 18:39:14 -0600

    all keys play a C note

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,41 +18,11 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func cPressed(_ sender: UIButton) {
+    @IBAction func keyPressed(_ sender: UIButton) {
         print("c")
         playSound()
     }
-    
 
-    @IBAction func dPressed(_ sender: UIButton) {
-        print("d")
-    }
-    
-    
-    @IBAction func ePressed(_ sender: UIButton) {
-        print("e")
-    }
-    
-    
-    @IBAction func fPressed(_ sender: UIButton) {
-        print("f")
-    }
-    
-    
-    @IBAction func gPressed(_ sender: UIButton) {
-        print("g")
-    }
-    
-    
-    @IBAction func aPressed(_ sender: UIButton) {
-        print("a")
-    }
-    
-    
-    @IBAction func bPressed(_ sender: UIButton) {
-        print("b")
-    }
-    
     
     func playSound() {
             let url = Bundle.main.url(forResource: "C", withExtension: "wav")
