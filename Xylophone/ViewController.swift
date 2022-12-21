2cdce0e70fde35138375a1215cccdde6d1c9292c
Kevin Pham <phamthk@gmail.com>
2022-12-20 20:20:53 -0600

    Fully working xylophone app with opacity functionality. Completed 12-20-22.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,29 +5,44 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
+//  Kevin Pham started following tutorial 12/19/22
+//
 
 import UIKit
-import AVFoundation
+import AVFoundation // used to play sound
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer!
+    var player: AVAudioPlayer! // 12-19-22 figure out why ?/!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    // called when any of the xylophone keys are pressed
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("c")
-        playSound()
+        
+      playSound(soundName: sender.currentTitle!)
+
+      //Reduces the sender's (the button that got pressed) opacity to half.
+      sender.alpha = 0.5
+
+      //Code should execute after 0.2 second delay.
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+          //Bring's sender's opacity back up to fully opaque.
+          sender.alpha = 1.0
+      }
     }
 
-    
-    func playSound() {
-            let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
             player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
+    }
+    
+    func dimKey() {
+        
     }
     
 }
